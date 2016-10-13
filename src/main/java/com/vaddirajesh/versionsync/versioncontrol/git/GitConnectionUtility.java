02ce1689fa6931d52b4684be0252f911f0581d50
rajesh.vaddi <rajesh.vaddi@foresee.com>
2016-10-13 16:15:25 -0700

    Added connection utility classes for git , svn and perforce

---
+++ src/main/java/com/vaddirajesh/versionsync/versioncontrol/git/GitConnectionUtility.java
@@ -0,0 +1,7 @@
+package com.vaddirajesh.versionsync.versioncontrol.git;
+
+/**
+ * Created by rajesh.vaddi on 10/13/16.
+ */
+public class GitConnectionUtility {
+}
---
+++ src/main/java/com/vaddirajesh/versionsync/versioncontrol/perforce/PerforceConnectionUtility.java
@@ -0,0 +1,7 @@
+package com.vaddirajesh.versionsync.versioncontrol.perforce;
+
+/**
+ * Created by rajesh.vaddi on 10/13/16.
+ */
+public class PerforceConnectionUtility {
+}
